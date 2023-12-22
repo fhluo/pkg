8995d1d5af88d4fa29ddda7f73d2efb20f9f405c
fhluo <20843637+fhluo@users.noreply.github.com>
2023-12-22 17:13:06 +0800

    fix: fix panic caused by type conversion failure

--- config/item.go
+++ config/item.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"github.com/spf13/cast"
 	"github.com/spf13/viper"
+	"log/slog"
 	"sync"
 )
 
@@ -24,7 +27,24 @@ func (item Item[T]) Get() T {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	return viper.Get(item.Key).(T)
+	value := viper.Get(item.Key)
+	switch value.(type) {
+	case []interface{}:
+		switch any(item.DefaultValue).(type) {
+		case []string:
+			value = cast.ToStringSlice(value)
+		case []int:
+			value = cast.ToIntSlice(value)
+		}
+	}
+
+	r, ok := value.(T)
+	if !ok {
+		slog.Warn("conversion failure", "type", fmt.Sprintf("%T", value))
+		return item.DefaultValue
+	}
+
+	return r
 }
 
 func (item Item[T]) Set(value T) {
